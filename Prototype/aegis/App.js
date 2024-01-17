61be94e951e9cf67d283cd6e2d6a68c4f46b5bc2
luciolle24 <luciolle24@gmail.com>
2024-01-17 15:57:16 +0100

    [Design Sprint] - Day 03 + More prototyping

--- Prototype/aegis/App.js
+++ Prototype/aegis/App.js
@@ -2,5 +2,34 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import SplashScreen from "./screens/splashScreen";
 
 export default function App() {
-    return <SplashScreen />;
+    return (
+        <View>
+            {/* <SplashScreen /> */}
+            <View style={styles.screen}>
+                <View style={styles.spacer} />
+                <Text>Rest</Text>
+                <View style={styles.spacer} />
+            </View>
+        </View>
+    );
 }
+const styles = StyleSheet.create({
+    screen: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#1d232f",
+    },
+
+    spacer: {
+        backgroundColor: "#5ce1e6",
+        height: 100,
+        width: "100%",
+    },
+    aegis: {
+        color: "#beeaee",
+        fontSize: 80,
+        textShadowColor: "#5ce1e6",
+        textShadowRadius: 40,
+        padding: 30,
+    },
+});
